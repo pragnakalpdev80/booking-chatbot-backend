17715ba85b9f35b2f65e61afae3045d04234c862
Hill Soni <pragnakalp.dev80@gmail.com>
2026-07-28 11:30:05 +0530

    fix(ci): add postgres and redis services to sonarqube workflow and update DB_HOST default

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
